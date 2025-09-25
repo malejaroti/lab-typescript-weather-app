1784ac0fccf94c6da7dbb40077448b38b8eb371b
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-09-25 15:27:59 +0200

    Main iterations done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,49 @@
+import { LocationResponse } from "./types"
+import { displayLocation, displayWeatherData, getCurrentWeather, getLocation } from "./utils"
+
 // src/main.ts
+const formNode = document.getElementById("weather-form") as HTMLFormElement
+
+// const handleSubmit2 = (e : Event) => {
+//     e.preventDefault()
+//     const locationInput = document.getElementById("location") as HTMLInputElement
+//     const locationName = locationInput.value
+//     console.log(`The user has submitted the form and is searching for ${locationName}`)
+//     getLocation(locationName)
+//         .then((response) => {
+//             if(response.results){
+//                 const location = response.results?.[0]
+//                 displayLocation(location)
+//                 return getCurrentWeather(location)
+//             }else{
+//                 throw new Error("Location not found")
+//             }
+//         })
+//         .then((responseWeatherApi) => {
+//             displayWeatherData(responseWeatherApi)
+//         })
+//         .catch((error) => {
+//             console.log("Error getting weather data")
+//             console.log(error)
+//         })
+// }
+
+const handleSubmit = async (e: Event) => {
+    e.preventDefault()
+    const locationInput = document.getElementById("location") as HTMLInputElement
+    const locationName = locationInput.value
+    console.log(`The user has submitted the form and is searching for ${locationName}`)
+
+    let locationResponse = await getLocation(locationName)
+    console.log("locations founds: ", location)
+    if(locationResponse.results){
+        let location = locationResponse.results[0]
+        displayLocation(location)
+        
+        let weatherResponse = await getCurrentWeather(location)
+        console.log("Weather Response: ", weatherResponse)
+        displayWeatherData(weatherResponse)
+    }
+}
+
+formNode.addEventListener("submit", handleSubmit)
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 // src/utils.ts
 
 import axios from 'axios';
-import { LocationResponse, Location } from "./types";
+import { LocationResponse, Location, WeatherResponse } from "./types";
 
 
 
@@ -10,5 +10,26 @@ export function getLocation(locationName: string): Promise<LocationResponse> {
     return axios.get(url).then((response) => response.data);
 }
 
+export function getCurrentWeather(locationDetails: Location): Promise<WeatherResponse> {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+    return axios.get(url).then((response) => response.data);
+}
 
+export function displayLocation(locationDetails: Location): void{
+    const locationNameNode = document.getElementById("location-name") as HTMLElement
+    locationNameNode.innerText = locationDetails.name
+    const countryNode = document.getElementById("country") as HTMLElement
+    countryNode.innerText = locationDetails.country
+}
+
+export function displayWeatherData(obj : WeatherResponse) :void{
+    const temperatureNode = document.getElementById("temperature") as HTMLElement
+    temperatureNode.innerText = `${obj.current_weather.temperature} ${obj.current_weather_units.temperature}`
+
+    const windSpeedNode  = document.getElementById("windspeed") as HTMLElement
+    windSpeedNode   .innerText = `${obj.current_weather.windspeed} ${obj.current_weather_units.windspeed}`
+
+    const winddirectionNode = document.getElementById("winddirection") as HTMLElement
+    winddirectionNode.innerText = `${obj.current_weather.winddirection} ${obj.current_weather_units.winddirection}`
+}
 
